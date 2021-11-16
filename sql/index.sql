61609620e95520e779adcbbc57b19f862518e0f3
songsu73 <songso0703@naver.com>
2021-11-16 18:28:23 +0900

    [유송수] 외래키형성_211116

--- sql/index.sql
+++ sql/index.sql
@@ -1,17 +1,22 @@
 -- XE
 -- Print browser.
 CREATE TABLE sec_show (
-  secid NUMBER, 
+  secid NUMBER not null,
   s1avg NUMBER,
   s2avg NUMBER,
   s3avg NUMBER,
   v1avg NUMBER,
   v2avg NUMBER,
   v3avg NUMBER,
-  damages VARCHAR2,
+  dangers VARCHAR2, --dangers로 통일함 ( 기존 damages)
   status VARCHAR2,
   notice VARCHAR2
 );
 
--- 컬럼 damages에 외래키를 설정하고 테이블을 연결할 필요가 있음.
+CREATE TABLE dangers(
+  secid NUMBER not null,
+  dangers VARCHAR2,
+  FOREIGN KEY('secid','dangers') REFERENCES 'sec_show' ('secid','dangers') --sec_show의 secid와 dangers 외래키 형성
+);
+-- 컬럼 dangers에 외래키를 설정하고 테이블을 연결할 필요가 있음.
 -- 더 필요한 요소가 있다면 주석 및 README 작성할 것.
