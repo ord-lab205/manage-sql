7dfbf19941491e3777e3f620a30c4d38331e00b7
bya2 <byaa1972@gmail.com>
2022-01-05 19:55:26 +0900

    .bat files

---
+++ bat/run-local-oracle-xe.bat
@@ -0,0 +1,4 @@
+@REM Windows CMD
+@REM DIR> ./bat/run-local-oracle-xe.bat
+@echo off
+sqlplus c##/lee@127.0.0.1/xe
---
+++ bat/run-oracle-xe.bat
@@ -0,0 +1,4 @@
+@REM Windows CMD
+@REM DIR> ./bat/run-oracle-xe.bat
+@echo off
+sqlplus c##lee/vs7954@210.119.146.61:1521/xe
